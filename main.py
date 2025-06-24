7a755853506af671c977f23160cb93807c069a88
nhquana2 <quannguyenhoang79@gmail.com>
2025-06-24 10:59:02 +0700

    fix main test

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from definition.vehicle import Vehicle
+from definition.board import Board
 
 def main():
     # Test Vehicle class
-    vehicle = Vehicle(id=1, length=2, orientation='horizontal', row=0, col=0)
+    vehicle = Vehicle(id=1, length=2, orientation='H', row=0, col=0)
 
 main()
